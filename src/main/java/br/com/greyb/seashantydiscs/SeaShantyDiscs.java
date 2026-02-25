68b55c33293f709f49e439aca3a42b9338d925b5
grey <grey.slm.m4rt1ins@gmail.com>
2026-02-25 14:23:21 -0300

    Added Barrett's Privateers, Bones in the Ocean, Leave Her Johnny and Rye Whiskey to a new pool in the shipwreck treasure loot table, and added Here's a Health to the Company, Rolling Down to Old Maui and Spanish Ladies to new pools in the big and small ocean ruins' loot tables

--- src/main/java/br/com/greyb/seashantydiscs/SeaShantyDiscs.java
+++ src/main/java/br/com/greyb/seashantydiscs/SeaShantyDiscs.java
@@ -27,13 +27,30 @@ public class SeaShantyDiscs implements ModInitializer {
                         .with(ItemEntry.builder(ModItems.MUSIC_DISC_FISH_IN_THE_SEA)));
             }
 
-            if (source.isBuiltin() && LootTables.BURIED_TREASURE_CHEST.equals(key)){
+            if (source.isBuiltin() && LootTables.BURIED_TREASURE_CHEST.equals(key)) {
                 LootPool.Builder poolBuilder = LootPool.builder().rolls(ConstantLootNumberProvider.create(1.0F))
                         .with(ItemEntry.builder(ModItems.MUSIC_DISC_CAPTAIN_KIDD))
                         .with(ItemEntry.builder(ModItems.MUSIC_DISC_RANDY_DANDY_O))
                         .with(ItemEntry.builder(ModItems.MUSIC_DISC_ROLL_THE_OLD_CHARIOT_ALONG))
                         .with(ItemEntry.builder(ModItems.MUSIC_DISC_SANTIANA));
+                tableBuilder.pool(poolBuilder);
+            }
 
+            if (source.isBuiltin() && LootTables.SHIPWRECK_TREASURE_CHEST.equals(key)) {
+                LootPool.Builder poolBuilder = LootPool.builder().rolls(ConstantLootNumberProvider.create(1.0F))
+                        .with(ItemEntry.builder(ModItems.MUSIC_DISC_BARRETTS_PRIVATEERS))
+                        .with(ItemEntry.builder(ModItems.MUSIC_DISC_BONES_IN_THE_OCEAN))
+                        .with(ItemEntry.builder(ModItems.MUSIC_DISC_LEAVE_HER_JOHNNY))
+                        .with(ItemEntry.builder(ModItems.MUSIC_DISC_RYE_WHISKEY));
+                tableBuilder.pool(poolBuilder);
+            }
+
+            if (source.isBuiltin() && (
+                    LootTables.UNDERWATER_RUIN_BIG_CHEST.equals(key) || LootTables.UNDERWATER_RUIN_SMALL_CHEST.equals(key))) {
+                LootPool.Builder poolBuilder = LootPool.builder().rolls(ConstantLootNumberProvider.create(1.0F))
+                        .with(ItemEntry.builder(ModItems.MUSIC_DISC_OLD_MAUI))
+                        .with(ItemEntry.builder(ModItems.MUSIC_DISC_SPANISH_LADIES))
+                        .with(ItemEntry.builder(ModItems.MUSIC_DISC_HERES_A_HEALTH_TO_THE_COMPANY));
                 tableBuilder.pool(poolBuilder);
             }
         }));
