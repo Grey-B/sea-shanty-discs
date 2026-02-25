17f8baec0782c7fda87eb9f9ca8de461bbc8da13
grey <grey.slm.m4rt1ins@gmail.com>
2026-02-25 14:01:25 -0300

    Added Captain Kidd, Randy Dandy-O, A Drop of Nelson's Blood and Santiana to a new pool in the buried chest loot table

--- src/main/java/br/com/greyb/seashantydiscs/SeaShantyDiscs.java
+++ src/main/java/br/com/greyb/seashantydiscs/SeaShantyDiscs.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTables;
 import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,6 +25,16 @@ public class SeaShantyDiscs implements ModInitializer {
                 tableBuilder.modifyPools((LootPool.Builder poolBuilder) -> poolBuilder
                         .with(ItemEntry.builder(ModItems.MUSIC_DISC_DRUNKEN_SAILOR))
                         .with(ItemEntry.builder(ModItems.MUSIC_DISC_FISH_IN_THE_SEA)));
+            }
+
+            if (source.isBuiltin() && LootTables.BURIED_TREASURE_CHEST.equals(key)){
+                LootPool.Builder poolBuilder = LootPool.builder().rolls(ConstantLootNumberProvider.create(1.0F))
+                        .with(ItemEntry.builder(ModItems.MUSIC_DISC_CAPTAIN_KIDD))
+                        .with(ItemEntry.builder(ModItems.MUSIC_DISC_RANDY_DANDY_O))
+                        .with(ItemEntry.builder(ModItems.MUSIC_DISC_ROLL_THE_OLD_CHARIOT_ALONG))
+                        .with(ItemEntry.builder(ModItems.MUSIC_DISC_SANTIANA));
+
+                tableBuilder.pool(poolBuilder);
             }
         }));
     }
