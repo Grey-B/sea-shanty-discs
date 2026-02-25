ae249ae28b88c8b6eca8f3ba2202d47585189614
grey <grey.slm.m4rt1ins@gmail.com>
2026-02-25 13:42:52 -0300

    Changed code to use modifyPools. Fishing loot table now works as intended

--- src/main/java/br/com/greyb/seashantydiscs/SeaShantyDiscs.java
+++ src/main/java/br/com/greyb/seashantydiscs/SeaShantyDiscs.java
@@ -2,33 +2,29 @@ package br.com.greyb.seashantydiscs;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.minecraft.loot.LootPool;
-import net.minecraft.loot.LootTable;
 import net.minecraft.loot.LootTables;
 import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.registry.RegistryKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.function.BiConsumer;
 
 public class SeaShantyDiscs implements ModInitializer {
     public static final String MOD_ID = "sea-shanty-discs";
 
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public void LootTableBiConsumer(BiConsumer<RegistryKey<LootTable>, LootTable.Builder> consumer) {
-        consumer.accept(
-                LootTables.FISHING_TREASURE_GAMEPLAY,
-                LootTable.builder().pool(LootPool.builder()
-                        .with(ItemEntry.builder(ModItems.MUSIC_DISC_DRUNKEN_SAILOR))
-                        .with(ItemEntry.builder(ModItems.MUSIC_DISC_FISH_IN_THE_SEA)))
-        );
-    }
-
     @Override
     public void onInitialize() {
         ModItems.initialize();
         ModMusic.initialize();
+
+        LootTableEvents.MODIFY.register(((key, tableBuilder, source, registries) -> {
+            if (source.isBuiltin() && LootTables.FISHING_TREASURE_GAMEPLAY.equals(key)) {
+                tableBuilder.modifyPools((LootPool.Builder poolBuilder) -> poolBuilder
+                        .with(ItemEntry.builder(ModItems.MUSIC_DISC_DRUNKEN_SAILOR))
+                        .with(ItemEntry.builder(ModItems.MUSIC_DISC_FISH_IN_THE_SEA)));
+            }
+        }));
     }
 }
