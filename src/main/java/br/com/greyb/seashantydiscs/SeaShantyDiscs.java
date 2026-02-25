102c1df0f3d75d64de59dd899a2ac64753476e35
grey <grey.slm.m4rt1ins@gmail.com>
2026-02-25 11:27:09 -0300

    Created LootTableBiConsumer to substitute adding to the loot pool. Regular vanilla loot now comes from the fishing treasure table. More testing is required

--- src/main/java/br/com/greyb/seashantydiscs/SeaShantyDiscs.java
+++ src/main/java/br/com/greyb/seashantydiscs/SeaShantyDiscs.java
@@ -2,7 +2,6 @@ package br.com.greyb.seashantydiscs;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTable;
 import net.minecraft.loot.LootTables;
@@ -11,25 +10,25 @@ import net.minecraft.registry.RegistryKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.function.BiConsumer;
+
 public class SeaShantyDiscs implements ModInitializer {
     public static final String MOD_ID = "sea-shanty-discs";
 
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    private static final RegistryKey<LootTable> FISHING_TABLE_ID = LootTables.FISHING_TREASURE_GAMEPLAY;
+    public void LootTableBiConsumer(BiConsumer<RegistryKey<LootTable>, LootTable.Builder> consumer) {
+        consumer.accept(
+                LootTables.FISHING_TREASURE_GAMEPLAY,
+                LootTable.builder().pool(LootPool.builder()
+                        .with(ItemEntry.builder(ModItems.MUSIC_DISC_DRUNKEN_SAILOR))
+                        .with(ItemEntry.builder(ModItems.MUSIC_DISC_FISH_IN_THE_SEA)))
+        );
+    }
 
     @Override
     public void onInitialize() {
         ModItems.initialize();
         ModMusic.initialize();
-
-        LootTableEvents.MODIFY.register(((registryKey, builder, lootTableSource, wrapperLookup) -> {
-            if (lootTableSource.isBuiltin() && FISHING_TABLE_ID.equals(registryKey)){
-                LootPool.Builder poolBuilder = LootPool.builder()
-                        .with(ItemEntry.builder(ModItems.MUSIC_DISC_DRUNKEN_SAILOR))
-                        .with(ItemEntry.builder(ModItems.MUSIC_DISC_FISH_IN_THE_SEA));
-                builder.pool(poolBuilder);
-            }
-        }));
     }
 }
